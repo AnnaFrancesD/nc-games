4d5e0c8acd4a48ad98730a2b9a18028261bab01e
Anna Dehl <annadehl@Annas-Macbook-Air.local>
2022-07-26 15:13:11 +0100

    display votes on commments

--- src/components/comment-card.jsx
+++ src/components/comment-card.jsx
@@ -2,6 +2,7 @@ export default function CommentCard({comment}) {
     return (
         <>
         <p>{comment.author}: {comment.body}</p>
+        <p>Votes: {comment.votes}</p>
         <p>on {comment.created_at.slice(0, -14)} at {comment.created_at.slice(-13, -8)}</p>
         </>
     )
