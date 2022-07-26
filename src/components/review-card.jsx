2b13a831584a94cb131c4f6560a3902333368250
Anna Dehl <annadehl@Annas-Macbook-Air.local>
2022-07-26 12:05:39 +0100

    add vote buttons

--- src/components/review-card.jsx
+++ src/components/review-card.jsx
@@ -28,13 +28,17 @@ export default function ReviewCard () {
             {isLoading ? (<p>Loading...</p>) : (
                <div key={currReview.review_id} className="review-card">
                    <h2>{currReview.title}</h2>
-                   <img src={currReview.review_img_url} alt={currReview.title} width="200"></img>
+                   <img className="review-img" src={currReview.review_img_url} alt={currReview.title}></img>
                    <p>Category: {formatCategoryString(currReview.category)}</p>
                    <p>Designer: {currReview.designer}</p>
                    <p>Owner: {currReview.owner}</p>
                    <p>Created at: {(currReview.created_at).slice(0, -14)}</p>
-                   <p>Votes: {currReview.votes}</p>
                    <p>{currReview.review_body}</p>
+                   <p>Votes: {currReview.votes}</p>
+                   <div className="vote-box">
+                   <button className="vote-button">👍</button>
+                   <button className="vote-button">👎</button>
+                   </div>
                </div>
         )}
         </div>
