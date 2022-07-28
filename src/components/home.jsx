82e85fa30ad23c555f70626c50fc637e3c0d6887
AnnaFrancesD <104217795+AnnaFrancesD@users.noreply.github.com>
2022-07-28 16:38:07 +0100

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,6 @@ import ReviewList from "./review-list";
 export default function Home() {
   return (
     <div>
-      <h2 className="welcome">Welcome to NC Games</h2>
       <ReviewList></ReviewList>
     </div>
   );
