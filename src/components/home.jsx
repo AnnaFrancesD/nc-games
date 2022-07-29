b0a416a820f4c58702b2da836bde5340b6a84351
Anna Dehl <annadehl@Annas-Macbook-Air.local>
2022-07-29 15:17:52 +0100

    add to homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,6 +3,10 @@ import ReviewList from "./review-list";
 export default function Home() {
   return (
     <div>
+      <p>
+        Welcome to NC Games! Looking for your next board game? Browse our latest
+        reviews here.
+      </p>
       <ReviewList></ReviewList>
     </div>
   );
